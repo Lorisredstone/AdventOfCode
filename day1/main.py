991a87d82b98e3ce9d2725d4ad4efc5459bef02f
Loris <monestierloris11@gmail.com>
2022-12-01 18:02:50 +0100

    day 1

---
+++ day1/main.py
@@ -0,0 +1,13 @@
+file = open("input.txt", "r")
+
+liste = []
+for elf in "".join(file.readlines()).split("\n\n"):
+    total = sum(int(nb) for nb in elf.split("\n"))
+    liste.append(total)
+
+# Part 1
+print(f"Part 1 : {max(liste)}")
+
+# Part 2
+new_liste = sorted(liste)
+print(f"Part 2 : {sum(new_liste[::-1][:3])}")
